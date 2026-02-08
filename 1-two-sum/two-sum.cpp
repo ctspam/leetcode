19b3c8980783a5681884dfec77f84ad2b406b08a
Chloe Pham <chloetpham25@gmail.com>
2026-02-07 19:40:54 -0800

    Time: 4 ms (53.86%) | Memory: 15 MB (18.85%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        //for this you want to start wihttt the first number and then use the complement to find if there's any matching
-        //lets create a hashmap for this
+        unordered_map<int, int> seen;
+        //lets say that you take the first number and see if the compliment is in the list, 
+        //so you say that 2 is the number and its complemenet is 9-2 = 7. so is 7 in the list?
+        //if not we move to the next index, and keep finding the same
 
-        unordered_map<int,int> findMap;
         for(int i = 0; i < nums.size(); ++i)
         {
-            //next you want to mark the first  num and find its complemenmmt
-            int num = nums[i];
-            int complement = target - num;
-            
-            //then you want to find if the complement even existst in the array
-            if(findMap.find(complement) != findMap.end()) return {findMap[complement], i};
-
-            findMap[num] = i;
+            int complement = target - nums[i];
+            if(seen.find(complement) != seen.end())
+            {
+                return{seen[complement], i};
+            }
+            seen[nums[i]] = i;
         }
-
         return {};
     }
 };
