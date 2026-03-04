e0b281368991d88017a11acb2559511b61d68777
Chloe Pham <chloetpham25@gmail.com>
2026-03-04 15:53:23 -0800

    Time: 63 ms (13.82%) | Memory: 34.4 MB (8.35%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -2,22 +2,23 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
 
-        unordered_map<string, vector<string>> groups;  
+        // unordered_map<string, vector<string>> groups;  
 
-        for (string s : strs)
-        {
-            string sortWord = s;
-            sort(sortWord.begin(), sortWord.end());
-            groups[sortWord].push_back(s);
-        }
+        // for (string s : strs)
+        // {
+        //     string sortWord = s;
+        //     sort(sortWord.begin(), sortWord.end());
+        //     groups[sortWord].push_back(s);
+        // }
 
-        vector<vector<string>> allGroups;
-        for (const auto& subGroup : groups )
-        {
-            allGroups.push_back(subGroup.second);
-        }
+        // vector<vector<string>> allGroups;
+        // for (const auto& subGroup : groups )
+        // {
+        //     allGroups.push_back(subGroup.second);
+        // }
         
-        return allGroups;
+        // return allGroups;
+
         // for loops are O(n), length k for sorting o(klogk)
         // TC = O(nk logk), SC = O(nk)
         // eat -> eat
@@ -27,6 +28,34 @@ public:
         // ant -> ttan, nat
         // abt -> bat
 
+        unordered_map<string, vector<string>> groups;  
+
+        for (string word : strs)
+        {
+            vector<int> count(26,0);
+            for(char c : word)
+            {
+                ++count[c-'a'];
+            }
+            
+            string key = "";
+            // turn this into a string
+            for (int num : count)
+            {
+                key += to_string(num)  + "|";
+            }
+            // 1|1|1|1|0|0 -> aet,eat, ate
+
+            groups[key].push_back(word);
+        }
+
+        vector<vector<string>> allGroups;
+        for (const auto& subGroup : groups )
+        {
+            allGroups.push_back(subGroup.second);
+        }
+        
+        return allGroups;
 
 
     }
