0d2ef42029e77d5532c254433524f1fe6abe098f
Chloe Pham <chloetpham25@gmail.com>
2026-03-04 17:40:11 -0800

    Time: 47 ms (15.80%) | Memory: 14.1 MB (66.29%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -4,17 +4,35 @@ public:
         unordered_map<int, int> seen;
         //lets say that you take the first number and see if the compliment is in the list, 
         //so you say that 2 is the number and its complemenet is 9-2 = 7. so is 7 in the list?
-        //if not we move to the next index, and keep finding the same
+        //if not we move to the next index, and keep finding the same,
 
-        for(int i = 0; i < nums.size(); ++i)
+        // for(int i = 0; i < nums.size(); ++i)
+        // {
+        //     int complement = target - nums[i];
+        //     if(seen.find(complement) != seen.end())
+        //     {
+        //         return{seen[complement], i};
+        //     }
+        //     seen[nums[i]] = i;
+        // }
+        // return {};
+
+        // so approaching this brute force, you want to find its complement
+        // and then find its complement and return if its found in the array
+
+        for (int i = 0; i < nums.size(); ++i)
         {
-            int complement = target - nums[i];
-            if(seen.find(complement) != seen.end())
+            for (int j = i+1; j < nums.size(); ++j)
             {
-                return{seen[complement], i};
+                int complement = target - nums[i];
+                if(complement == nums[j])
+                {
+                    return {i,j};
+                }
             }
-            seen[nums[i]] = i;
         }
         return {};
+
+        // TC O(n^2), SC = O(1)
     }
 };
