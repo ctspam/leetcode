64b96204b0ee2a42d7aff8f3e8d5752557ae915d
Chloe Pham <chloetpham25@gmail.com>
2026-03-05 18:42:17 -0800

    Time: 3 ms (9.04%) | Memory: 19.7 MB (33.13%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -2,16 +2,28 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         int n=numbers.size();
-        unordered_map<int,int> mp;
-        for(int i=0;i<n;i++) {
-            mp[numbers[i]]=i;
-        }
 
         for(int i=0;i<n;i++) {
             int comple=target-numbers[i];
-            if(mp.find(comple)!=mp.end()) {
-                if((comple==numbers[i]&&mp[comple]!=i)||comple!=numbers[i])
-                return {min(mp[comple]+1,i+1),max(mp[comple]+1,i+1)};
+            // binary search for non-decreasing
+            int lo=-1, hi=n-1;
+            while(lo<hi) {
+                int mid=lo+(hi-lo+1)/2;
+                if(numbers[mid]<=comple) lo=mid;
+                else hi=mid-1; 
+            }
+            if(lo!=-1&&numbers[lo]==comple) {
+                // cout<<i<<" "<<lo<<" "<<numbers[i]<<" "<<comple<<'\n';
+                if(comple==numbers[i]) {
+                    // right-most 
+                    if(lo==i) {
+                        if(lo-1>=0) {
+                            if(numbers[lo-1]==numbers[i]) return {min(lo+1,i+1),max(lo+1,i+1)};
+                        } else return {};
+                    } else return {min(lo+1,i+1),max(lo+1,i+1)};
+                } 
+                else 
+                return {min(lo+1,i+1),max(lo+1,i+1)};
             }
         }
 
