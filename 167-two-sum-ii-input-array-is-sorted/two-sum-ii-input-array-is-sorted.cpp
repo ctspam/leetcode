d9ac8af19ae5f729cdf37fa2bf137c4eca565f3a
Chloe Pham <chloetpham25@gmail.com>
2026-03-05 18:30:49 -0800

    Time: 0 ms (100.00%) | Memory: 19.7 MB (33.13%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        int n=numbers.size()/2;
-        if (n%2 != 1) n--;
-        //unordered_set<int> sett = ;
-        //int i = 0;
-        int left= 0;
-        int right = numbers.size()-1;
-        while (left!= right) 
-        {
-             
-            if (numbers[left] + numbers[right] == target) return {left+1, right+1};
-            else if (numbers[left] + numbers[right] > target)
-            {
-                right--;
-            }
-            else if (numbers[left] + numbers[right] < target )
-            {
-                left++;
-            }
-            else
-            {
-                left++;
-                right--;
+        int n=numbers.size();
+        unordered_map<int,int> mp;
+        for(int i=0;i<n;i++) {
+            mp[numbers[i]]=i;
+        }
+
+        for(int i=0;i<n;i++) {
+            int comple=target-numbers[i];
+            if(mp.find(comple)!=mp.end()) {
+                if((comple==numbers[i]&&mp[comple]!=i)||comple!=numbers[i])
+                return {min(mp[comple]+1,i+1),max(mp[comple]+1,i+1)};
             }
         }
+
+        //unordered_set<int> sett = ;
+        //int i = 0;
         return {};
     }
 };
