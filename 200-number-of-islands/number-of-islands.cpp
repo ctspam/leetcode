796e355d1877f3686506c979b2ba74a526577a30
Chloe Pham <chloetpham25@gmail.com>
2026-03-07 01:07:03 -0800

    Time: 26 ms (63.26%) | Memory: 16.1 MB (89.40%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,44 +1,54 @@
 class Solution {
 public:
-    int numIslands(vector<vector<char>>& grid) 
+    int numIslands(vector<vector<char>>& grid)
     {
-        // check the cases first
+        // in the upper one u want to traverse the grid
+        // first u want to check if there are anything in the grid
+
         if(grid.size() == 0 || grid[0].size() == 0)
         {
             return 0;
         }
 
+        // now u want to keep track of the num of islands
         int count = 0;
-        for(int i = 0; i < grid.size(); ++i)
+
+        for (int i = 0; i < grid.size(); ++i)
         {
-            for(int j = 0; j < grid[0].size(); ++j)
+            for (int j = 0; j < grid[0].size(); ++j)
             {
-                if(grid[i][j] == '1')
+                // now check that if it does have a 1 or not
+                if (grid[i][j] == '1')
                 {
                     dfs(grid, i, j);
                     ++count;
-                }   
+                }
             }
         }
-
         return count;
     }
 
-private: void dfs(vector<vector<char>>& grid, int i, int j)
+private: 
+    void dfs(vector<vector<char>> &grid, int i, int j)
     {
-        // these check if the dfs is going to be out of bounds when traversing
-        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == '0')
+        // this is the dfs 
+        // so we first need to check if its out of bounds when traversing
+        // so we can avoid of all of that
+
+        if (i < 0 || i >= grid.size() ||
+            j < 0 || j >= grid[0].size() ||
+            grid[i][j] == '0')
         {
             return;
         }
 
-        // this will mark the cells visited
+        // now everytime we visit a '1' we want to mark it as '0'
         grid[i][j] = '0';
 
-        // NOW explore all four directions
-        dfs(grid, i+1, j);
-        dfs(grid, i-1, j);
-        dfs(grid, i, j+1);
-        dfs(grid, i, j-1);
+        // now we grow our search in the dfs going top down right left
+        dfs(grid, i-1, j); // top
+        dfs(grid, i+1, j); // bottom
+        dfs(grid, i, j-1); // right
+        dfs(grid, i, j+1); // left
     }
 };
