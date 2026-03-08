f33435afbad9649a49723550d948ff368178b61c
Chloe Pham <chloetpham25@gmail.com>
2026-03-07 22:43:29 -0800

    Time: 0 ms (100.00%) | Memory: 10 MB (10.23%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,26 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
+        // return s == t;
 
-        return s == t;
+        if (s.size() != t.size())
+            return false;
+
+        unordered_map<char, int> countS;
+        unordered_map<char, int> countT;
+
+        for (int i = 0; i < s.size(); ++i)
+        {
+            // a:3, n:1, etcetc
+            ++countS[s[i]];
+            ++countT[t[i]];
+        }
+
+        // equal amount of the letters 
+        return countS == countT;
+
+
     }
 };
