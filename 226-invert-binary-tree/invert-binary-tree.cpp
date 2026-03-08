cd0f2646828882e599a12074dde96f3462694219
Chloe Pham <chloetpham25@gmail.com>
2026-03-07 18:04:52 -0800

    Time: 0 ms (100.00%) | Memory: 12.4 MB (61.03%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.cpp
+++ 226-invert-binary-tree/invert-binary-tree.cpp
@@ -17,10 +17,19 @@ public:
         if(!root)
             return nullptr;
 
-        swap(root->right, root->left);
+        queue<TreeNode*> q;
+        q.push(root);
 
-        invertTree(root->left);
-        invertTree(root->right);
+        while(!q.empty())
+        {
+            TreeNode* node = q.front();
+            q.pop();
+
+            swap(node->left, node->right);
+
+            if(node->left) q.push(node->left);
+            if(node->right) q.push(node->right);
+        }
 
         return root;
     }
